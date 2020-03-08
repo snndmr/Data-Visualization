b7db11154c2f683598cc5f5f472a8ad7683daafb
SNN <snndmr@users.noreply.github.com>
2020-03-08 19:39:46 +0300

    updated draw func

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import pandas
 from matplotlib import pyplot, ticker
 
-excel_data = pandas.read_excel('dataOne.xlsx')
-ai = excel_data['AI'].tolist()
-test = excel_data['TEST'].tolist()
+COLORS = ['#FF6666', '#AAAAAA', '#000000']
+
 
 # To draw graph
 
-COLORS = ['#FF6666', '#AAA', '#000']
-
-
-def draw_background(y, init, end):
-    pyplot.hlines(y, init, end, colors=COLORS[0 if y == 1 else 1], lw=40)
-
-
-def draw_detected(y, init, end):
-    pyplot.hlines(y, init, end + 1, colors=COLORS[2], lw=40)
-
-
 def draw_line(y, intervals):
-    draw_background(y, 0, len(excel_data))
-    for i, j in intervals[1]:
-        draw_detected(y, i, j)
+    pyplot.hlines(y, 0, len(excel_data), colors=COLORS[0 if y == 1 else 1], lw=40)  # To draw background
+    for i, j in intervals:
+        pyplot.hlines(y, i, j + 1, colors=COLORS[2], lw=40)
 
 
 # To get data.
@@ -31,25 +19,19 @@ def get_ai_intervals(ai_list):
     counter = 0
     intervals = list()
 
-    for _ in range(2):
-        intervals.append(list())
-
     for order, data in enumerate(ai_list):
         data = 1 if data >= 0.5 else 0
 
         if counter > 0 and index != data:
-            intervals[index].append([order - counter, order - 1])
+            intervals.append([order - counter, order - 1])
             counter = 0
 
         if data == 1:
             index = 1
             counter += 1
-        else:
-            index = 0
-            counter += 1
 
-        if order == len(ai_list) - 1 and counter > 0:
-            intervals[index].append([order - counter + 1, order])
+        if order == len(ai_list) - 1 and counter > 0 and index == 1:
+            intervals.append([order - counter + 1, order])
     return intervals
 
 
@@ -58,25 +40,19 @@ def get_test_intervals(test_list):
     counter = 0
     intervals = list()
 
-    for _ in range(2):
-        intervals.append(list())
-
     for order, data in enumerate(test_list):
         data = 1 if data == "'drone'" else 0
 
         if counter > 0 and index != data:
-            intervals[index].append([order - counter, order - 1])
+            intervals.append([order - counter, order - 1])
             counter = 0
 
         if data == 1:
             index = 1
             counter += 1
-        else:
-            index = 0
-            counter += 1
 
-        if order == len(test_list) - 1 and counter > 0:
-            intervals[index].append([order - counter + 1, order])
+        if order == len(test_list) - 1 and counter > 0 and index == 1:
+            intervals.append([order - counter + 1, order])
     return intervals
 
 
@@ -85,26 +61,20 @@ def get_comparision_intervals(ai_list, test_list):
     counter = 0
     intervals = list()
 
-    for _ in range(2):
-        intervals.append(list())
-
     for order, (ai_data, test_data) in enumerate(zip(ai_list, test_list)):
         x = 1 if ai_data >= 0.5 else 0
         y = 1 if test_data == "'drone'" else 0
 
         if counter > 0 and index != (1 if x == y else 0):
-            intervals[index].append([order - counter, order - 1])
+            intervals.append([order - counter, order - 1])
             counter = 0
 
         if x == y:
             index = 1
             counter += 1
-        else:
-            index = 0
-            counter += 1
 
-        if order == len(test_list) - 1 and counter > 0:
-            intervals[index].append([order - counter + 1, order])
+        if order == len(test_list) - 1 and counter > 0 and index == 1:
+            intervals.append([order - counter + 1, order])
     return intervals
 
 
@@ -120,6 +90,10 @@ def formatter(y, pos):
     else:
         return ""
 
+
+excel_data = pandas.read_excel('dataOne.xlsx')
+ai = excel_data['AI'].tolist()
+test = excel_data['TEST'].tolist()
 
 draw_line(3, get_ai_intervals(ai))
 draw_line(2, get_test_intervals(test))
