4c43616c6e49a7858a77f3be957626df4cc54f6d
SNN <snndmr@users.noreply.github.com>
2020-03-10 20:51:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ from matplotlib import pyplot as plt, ticker
 COLORS = ['white', 'gray', 'black']
 
 
-def draw_line(y, intervals):
+def draw_lines(y, intervals):
     plt.hlines(y, 0, len(excel_data), colors=COLORS[0 if y == 1 else 1], lw=40)  # To draw the background.
     for i, j in intervals:
         plt.hlines(y, i, j + 1, colors=COLORS[2], lw=40)
+
+
+def draw_info(info_list):
+    for i in info_list:
+        order, pos, init, end, text = i.split(',')
+        draw_up_bracket(int(order), int(init), int(end), text) if int(pos) == 1 \
+            else draw_down_bracket(int(order), int(init), int(end), text)
+
+
+def draw_up_bracket(y, init, end, text):
+    plt.vlines((init + end) / 2, y + .25, y + .35, colors='black', lw=1)
+    plt.vlines(init, y + .15, y + .25, colors='black', lw=1)
+    plt.vlines(end, y + .15, y + .25, colors='black', lw=1)
+    plt.hlines(y + .25, init, end, colors='black', lw=1)
+    plt.text((init + end) / 2, y + .4, text, fontsize=12, horizontalalignment='center')
+
+
+def draw_down_bracket(y, init, end, text):
+    plt.vlines((init + end) / 2, y - .25, y - .35, colors='black', lw=1)
+    plt.vlines(init, y - .15, y - .25, colors='black', lw=1)
+    plt.vlines(end, y - .15, y - .25, colors='black', lw=1)
+    plt.hlines(y - .25, init, end, colors='black', lw=1)
+    plt.text((init + end) / 2, y - .45, text, fontsize=12, horizontalalignment='center')
 
 
 def get_intervals(data_list):  # To get data from columns.
@@ -60,23 +83,25 @@ def formatter(y, pos):
         return 'Test Data'
     elif y == 1:
         return 'Comparison'
-    else:
+    elif y == pos:
         return ''
 
 
-excel_data = pandas.read_excel('dataOne.xlsx')
-ai = excel_data['AI'].tolist()
-test = excel_data['TEST'].tolist()
+excel_data = pandas.read_excel('dataFive.xlsx', header=None)
+ai = excel_data[0].tolist()
+test = excel_data[1].tolist()
+info = excel_data[2].dropna().tolist()
 
-draw_line(3, get_intervals(ai))
-draw_line(2, get_intervals(test))
-draw_line(1, get_comparision_intervals(ai, test))
+draw_info(info)
+draw_lines(3, get_intervals(ai))
+draw_lines(2, get_intervals(test))
+draw_lines(1, get_comparision_intervals(ai, test))
 
-plt.ylim([0, 4])
+plt.ylim([0, 4]), plt.yticks(fontsize=18), plt.xticks(fontsize=18)
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(formatter))
 
 plt.plot(0, 0, COLORS[2], label='Drone', lw=5)
 plt.plot(0, 0, COLORS[1], label='No Drone', lw=5)
-plt.legend(prop={'size': 20}), plt.yticks(fontsize=18), plt.xticks(fontsize=18)
+plt.legend(prop={'size': 20})
 
 plt.show()
